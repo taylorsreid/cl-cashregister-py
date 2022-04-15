51762decebd11b7a3431fba303ad4ec60cfbcbb5
taylorsreid <taylorsreid@gmail.com>
2022-04-15 02:38:10 -1000

    Cleaned up code, fixed bugs, start on printtrans() and the JSON current transaction file next.

--- crfunc.py
+++ crfunc.py
@@ -11,11 +11,11 @@ allitems = {} #DICTIONARY OF DICTIONARIES TO STORE ALL ITEMS IN THE TRANSACTION?
 format for allitems:
 {
     "upc1" : {
-        "name" : ""
+        "item1name" : ""
         "price" : 13.99
     }
     "upc2" : {
-        "name" : ""
+        "item2name" : ""
         "price" : 69.99
     }
 }
@@ -30,36 +30,53 @@ def clearscreen():
     else:
         _ = system('clear')
 
-def additem():
+def usrinp():
+    print("Scan a UPC to add an item | Void (I)tem | Void (T)ransaction | (Q)uit")
+    selection = input()
+    match selection:
+        case "i":
+            voiditem()
+        case "t":
+            voidtrans()
+        case "q":
+            yn = input("Are you sure you want to quit? Y for yes, N for no")
+            if yn.lower() == "y":
+                print("Goodbye and thanks for using Python Command Line Cash Register!")
+                exit()
+                os.system("pause")
+        case _ :
+            additem(selection)
+
+def additem(selection):
     #ADD METHOD TO ADD AN ITEM TO THE DICTIONARY OF CURRENT ITEMS IN THE TRANSACTION
     #STORE DICTIONARY IN A JSON FILE SO THAT ITEMS NOT FOUND IN THE API CAN BE DEFINED AND REMEMBERED FOR A LATER DATE
     #AND SO THAT THE API ISN'T OVERUSED
     #MIGRATE TO AN SQL DATABASE LATER
 
-    #ITOEN GREEN TEA UPC FOR TESTING: 073366118238
-    global upc; global itemname
-    upc = input("Please input a UPC:  ")
-    response = requests.request("GET", f"https://barcode.monster/api/{upc}").json()
-    itemname = list(response["description"]) #puts the item name into a list so it can be modified
+    global itemname, upc
+
+    response = requests.request("GET", f"https://barcode.monster/api/{selection}").json()
+    tempitemname = list(response["description"]) #puts the item name into a list so it can be modified
 
     #23 chars to remove the "(from barcode.monster)" blah blah blah
     i = -23
     while i < 0:
-        del itemname[i]
+        del tempitemname[i]
         i += 1
 
     #makes the itemname list back into a string
-    itemname = ''.join(itemname)
+    itemname = ''.join(tempitemname)
+    upc = selection
 
 def printtrans(): #PRINTS THE TRANSACTION TO THE SCREEN
-    clearscreen()
-
-    print("Scan a UPC to add an item | (R)emove an item | (V)oid Transaction | (Q)uit")
-
     termsize = os.get_terminal_size().columns
     spaces = termsize - (len(upc) + len(itemname))
     print(upc + (" " * spaces) + itemname)
 
-def removeitem():
+def voiditem():
     #ADD METHOD TO REMOVE AN ITEM FROM THE DICTIONARY OF CURRENT ITEMS IN THE TRANSACTION
+    pass
+
+def voidtrans():
+    #ADD METHOD TO CLEAR DICTIONARY AND START OVER
     pass
--- start.py
+++ start.py
@@ -1,19 +1,16 @@
+#ITOEN GREEN TEA UPC FOR TESTING: 073366118238
+
 import crfunc #my functions file
 import os
 mastercontrolbool = True #master control whether to continue looping or not
+version = "alpha"
 
 crfunc.clearscreen()
-print("Hello and welcome to the Python Command Line Cash Register")
+print(f"Hello and welcome to the Python Command Line Cash Register")
+print(f"Version {version}")
 
 while mastercontrolbool == True:
 
-    crfunc.additem()
+    crfunc.usrinp()
+    crfunc.clearscreen()
     crfunc.printtrans()
-
-    runagain = input("Would you like to do another item?  y for yes, n for no:  ")
-    if runagain.lower() != "y":
-        mastercontrolbool = False
-    else:
-        crfunc.clearscreen()
-        print("GOODBYE!")
-        os.system("pause") #stops the program from ending immediately after the last action
